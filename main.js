79efeef1d88dc8dca6634999842e326e5f7e7adb
tashkulov <nurislamtashkulov168@gmail.com>
2023-10-19 08:45:04 +0600

    Fixed js
    updated

--- main.js
+++ main.js
@@ -87,19 +87,145 @@
 // }
 // console.log(sum)
 
-//task 3
-const obj={
-    1:'Korea',
-    2:"Kyrgyzstan",
-    3:"Dubai",
-    4:'Singapur',
-    5:'Oslo'
-}
+// task 3
+// const obj={
+//     1:'Korea',
+//     2:"Kyrgyzstan",
+//     3:"Dubai",
+//     4:'Singapur',
+//     5:'Oslo'
+// }
 
-for(const key in obj){
-    if(obj[key].length>=8){
-        console.log(obj[key])
-    }
-}
+// for(const key in obj){
+//     if(obj[key].length>=8){
+//         console.log(obj[key])
+//     }
+// }
 // console.log(`${obj[2]}
 // ${obj[4]}`)
+
+//while
+// let i =0
+// while(i<10){
+//     console.log(i)
+//     i++
+// }
+
+//do while
+// let i =0
+// do{
+//     console.log(i)
+//     i++
+// }while(i<10)
+
+
+
+
+//task1 
+
+// Нужно написать цикл, который перебирает числа от 4 до 37 и выполняет следующие действия:
+
+// Если число кратно 2, но не кратно 4, то прибавляет его к результату;
+// Если число кратно 3, то отнимает его от результата;
+// Если число кратно 4, то умножает его на результат;
+// Если число кратно 5, то просто выводит в консоль это число и не изменяет результат (даже если число подпадает под условия выше);
+// Если число не попадает под условия выше, то пропускает его.
+// Число должно попадать только под одно условие. Если число пападает под несколько условий - выполнится должно первое из них.
+
+
+// let result =0
+// let startNum=4
+// for(let i = startNum;i<=37;i++){
+//     if(i%5==0){
+//         console.log(i)
+//         continue;
+//     } if(i%4===0){
+//         result*=i
+//         // console.log(i)
+
+//         continue
+//     } if(i%3===0){
+//         result-=i
+//         // console.log(i)
+
+//         continue
+//     } if(i%2===0 && i % 4 !== 0){
+//         result+=i
+//         // console.log(i)
+//         continue;
+//     }
+// }
+//  console.log(result)
+
+//task2 
+
+// let sum=100
+// let result= 0
+// for(let i=sum;i>0;i--){
+//     result+=i
+// }
+// console.log(result)
+
+//task 3
+
+// let result=0
+// for(let i =0;i<=100;i+=2){
+//     console.log(i)
+// }
+
+// const arr = [3,6,1,234,456,765,13413,4564563,46545642]
+// for(let i =0;i<arr.length;i++){
+//     if(arr[i]%2===0){
+//         console.log(arr[i])
+//     }
+// }
+
+// let palindrom = 'asddsa'
+// let isValid = false
+
+// for(let i = 0; i < palindrom.length / 2; i++) {
+//     if(palindrom.length % 2 === 1) {
+//         console.log('Не палиндром')
+//         break;
+//     }
+
+//     if(palindrom[i] === palindrom[(palindrom.length - 1) - i]) {
+//         isValid = true
+//     } else {
+//         isValid = false
+//         break
+//     }
+// }
+
+// if(isValid) {
+//     console.log('Палиндром')
+// } else {
+//     console.log('Не палиндром')
+// }
+
+// let factorial=5
+// let res=1
+// for(let i =1;i<=factorial;i++){
+//         res*=i
+
+// }
+// console.log(res)
+
+const romanNum={
+    I:1,
+    V:5,
+    X:10,
+    L:50
+
+}
+let result=''
+let num=25
+for(const key in romanNum){
+    while(num>=romanNum[key]){
+        result+=key
+        num-=romanNum[key]
+
+    }
+}
+
+console.log(result)
